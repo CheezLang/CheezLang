022cb3d1c368834ec1ae79337e345b6f83af464a
Nico Otremba <nico.otremba@mmi-media.de>
2019-01-07 13:23:24 +0100

    added files

---
+++ LLVM/TestNet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using LLVMCS;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var module = new Module("deine mudda");
+
+            module.SetTargetTriple("i386-pc-win32");
+
+            var tt = module.GetTargetTriple();
+
+            var dl = new DataLayout(module);
+
+            var str = module.Context.GetNamedStruct("test_struct");
+            module.Context.SetStructBody(str, new TypeRef[]
+            {
+                module.Context.GetIntType(32),
+                module.Context.GetIntType(64),
+                module.Context.GetIntType(1),
+                module.Context.GetFloatType(32).GetPointerTo()
+            });
+
+            module.PrintToFile("test.ll");
+
+            dl.Dispose();
+            module.Dispose();
+            Console.WriteLine("ok");
+        }
+    }
+}
