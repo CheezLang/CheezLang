032bcb2467cdf909ef4a33173d9aa2826ee7ebf4
Nico Otremba <nico.otremba@mmi-media.de>
2019-11-25 19:35:49 +0100

    stuff

--- scripts/run_program.ps1
+++ scripts/run_program.ps1
@@ -7,8 +7,8 @@ param (
 
 Push-Location gen
 # &"./$file$executable_file_extension" compile -files test.che
-# &"./$file$executable_file_extension" project new -name hello_world -type Program
-&"./$file$executable_file_extension" help
+&"./$file$executable_file_extension" project new -name hello_world3 -type program
+# &"./$file$executable_file_extension" help
 # &"./$file$executable_file_extension" run -file main.che
 # &"./$file$executable_file_extension"
 Pop-Location
