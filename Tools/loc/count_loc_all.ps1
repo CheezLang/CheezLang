6ca23ed0243007dd78e7b4a59e91b10c076f292d
Nico Otremba <nico.otremba@mmi-media.de>
2019-11-25 17:38:50 +0100

    loc

--- Tools/loc/count_loc_all.ps1
+++ Tools/loc/count_loc_all.ps1
@@ -6,9 +6,11 @@
 .\count_loc.ps1 ..\..\Backends\LLVM\LLVMWrapper\src .\Backends_LLVM_LLVMWrapper.csv  -exclude TemporaryGeneratedFile*
 .\count_loc.ps1 ..\..\Backends\LLVM\TestNet .\Backends_LLVM_TestNet.csv *.cs -exclude TemporaryGeneratedFile*
 .\count_loc.ps1 ..\..\CompilerCLI .\CompilerCLI.csv *.cs -exclude TemporaryGeneratedFile*
-.\count_loc.ps1 ..\..\examples .\che_all.csv *.che
-.\count_loc.ps1 ..\..\examples\examples .\examples.csv *.che
-.\count_loc.ps1 ..\..\examples\libraries .\libraries.csv *.che
-.\count_loc.ps1 ..\..\examples\std .\std.csv *.che
+.\count_loc.ps1 ..\..\examples .\cheez_all.csv *.che
+.\count_loc.ps1 ..\..\examples\examples .\cheez_examples.csv *.che
+.\count_loc.ps1 ..\..\examples\libraries .\cheez_libraries.csv *.che
+.\count_loc.ps1 ..\..\examples\libraries\compiler .\cheez_compiler.csv *.che
+.\count_loc.ps1 ..\..\examples\std .\cheez_std.csv *.che
+.\count_loc.ps1 ..\..\examples\tests .\cheez_tests.csv *.che
 .\count_loc.ps1 ..\LanguageServer\VSCodeExtension\src .\Tools_LanguageServer_VSCodeExtension.csv *.ts
 .\count_loc.ps1 ..\LanguageServer\Server .\Tools_LanguageServer_Server.csv *.cs -exclude TemporaryGeneratedFile*
