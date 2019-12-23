a50eec5a76a80122569b8ccf554d62c6f11578a7
Nico Otremba <nico.otremba@mmi-media.de>
2019-12-23 20:45:56 +0100

    new imgui binding

---
+++ Tools/Bindings/imgui_binding_all.cpp
@@ -0,0 +1,3 @@
+#include "D:/Programming/CppLibs/imgui/imgui/imgui.cpp"
+#include "D:/Programming/CppLibs/imgui/imgui/examples/imgui_impl_opengl3.h"
+#include "D:/Programming/CppLibs/imgui/imgui/examples/imgui_impl_glfw.h"
