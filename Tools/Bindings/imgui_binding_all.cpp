abb7fdf064ac346c6fd51ea1b8b034dbe987e96e
Nico Otremba <nico.otremba@mmi-media.de>
2019-12-24 18:16:41 +0100

    imgui now working with docking branch

--- Tools/Bindings/imgui_binding_all.cpp
+++ Tools/Bindings/imgui_binding_all.cpp
@@ -1,3 +1,4 @@
 #include "D:/Programming/CppLibs/imgui/imgui/imgui.h"
+#include "D:/Programming/CppLibs/imgui/imgui/imgui_internal.h"
 #include "D:/Programming/CppLibs/imgui/imgui/examples/imgui_impl_opengl3.h"
 #include "D:/Programming/CppLibs/imgui/imgui/examples/imgui_impl_glfw.h"
