670e8a368a5710b6da1a6dc5a9aa0ead46bc46d8
Nico Otremba <nicolas.otremba@web.de>
2019-12-27 01:58:11 +0100

    Bindings: updated glad binding

---
+++ Tools/Bindings/glad/glad_binding.lua
@@ -0,0 +1,23 @@
+source_file = "glad_binding_source.cpp"
+
+function prepend_to_cpp()
+	return [[
+#include <memory>
+
+extern "C" int gladLoadGL(void);
+extern "C" int gladLoadGLLoader(GLADloadproc);
+
+]]
+end
+
+function on_global_variable(decl, name, type)
+	index = name:find("glad_gl")
+	if (index == nil)
+	then
+		return false, nil
+	else
+		sub_name = name:sub(6)
+		result = sub_name .. " : " .. Type.to_cheez_string(type) .. " #extern #linkname(\"" .. name .. "\")"
+		return true, result
+	end
+end
