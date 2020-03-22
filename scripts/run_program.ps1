5799d9a1465bf12142a4e0c446614215c3e9c047
Nico Otremba <>
2020-03-22 22:12:18 +0100

    config: stuff

--- scripts/run_program.ps1
+++ scripts/run_program.ps1
@@ -15,7 +15,7 @@ Push-Location gen
 # &"./$file$executable_file_extension" find-tokens -file D:\Programming\CheezLang\gen\uiae.che -type StringLiteral -suffix "c"
 # &"./$file$executable_file_extension" compile -files test.che
 # &"./$file$executable_file_extension" project new -name hello_world3 -type program
-&"./$file$executable_file_extension" help
+&"./$file$executable_file_extension" "../data/json_test.json"
 # &"./$file$executable_file_extension" run -file main.che
 Pop-Location
 
