45ec0bc07971015979c3ac3843b6bb10988a2f45
Nico Otremba <>
2020-03-24 14:46:29 +0100

    bug fixes and more

--- scripts/run_program.ps1
+++ scripts/run_program.ps1
@@ -15,8 +15,8 @@ Push-Location gen
 # &"./$file$executable_file_extension" find-tokens -file D:\Programming\CheezLang\gen\uiae.che -type StringLiteral -suffix "c"
 # &"./$file$executable_file_extension" compile -files test.che
 # &"./$file$executable_file_extension" project new -name hello_world3 -type program
-&"./$file$executable_file_extension" "../data/json_test.json"
-# &"./$file$executable_file_extension" run -file main.che
+# &"./$file$executable_file_extension" "../data/json_test.json"
+&"./$file$executable_file_extension" help
 Pop-Location
 
 Write-Host "Program exited with code $LASTEXITCODE"
