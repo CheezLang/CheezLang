e16eed584a53e10da1ad91cbd63beebe9f8f2cad
Nico Otremba <>
2020-03-27 23:52:03 +0100

    cheez compiler in debugger working

--- scripts/build_debugger.ps1
+++ scripts/build_debugger.ps1
@@ -1,2 +1,3 @@
 . ./scripts/config.ps1
-&$cheezc examples/libraries/compiler/debugger.che -o ./gen -n cheezdb --stdlib ./examples --time --error-source
+&$cheezc examples/libraries/compiler/debugger.che -o ./gen -n cheezdb --stdlib ./examples --time --error-source --opt
+Write-Host "Done"
