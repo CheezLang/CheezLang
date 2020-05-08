502d4a398d49881d0eecf2e662876c7ea1b6acac
Nimaoth <nicolas.otremba@web.de>
2020-05-08 19:09:47 +0200

    Moved projects into seperate repositories

--- Tools/VSCodeExtension/config.ps1
+++
@@ -1,1 +0,0 @@
-$extensionName = "cheez"
--- examples/std/os/windows_macros/WindowsMacros/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include "Windows.h"
-
-extern "C" void* winmacro_GetFiberData() {
-    return GetFiberData();
-}
-
-extern "C" void* winmacro_GetCurrentFiber() {
-    return GetCurrentFiber();
-}
