b6421175f4ecef3155c550e832c9e5cd833d10e9
Nimaoth <nicolas.otremba@web.de>
2020-05-13 14:19:40 +0200

    copy files in ps1 file

---
+++ scripts/copy_additional_build_files.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $solution_dir,
+
+    [Parameter()]
+    [string]
+    $project_dir,
+
+    [Parameter()]
+    [string]
+    $out_dir
+)
+
+$solution_dir = $solution_dir.Replace("\\", "/")
+$project_dir = $project_dir.Replace("\\", "/")
+$out_dir = $out_dir.Replace("\\", "/")
+
+Write-Output "copy additional files to $project_dir/$out_dir"
+
+Copy-Item "$solution_dir/modules.txt" -Destination "$project_dir/$out_dir" -Force
+Copy-Item "$solution_dir/lib" -Destination "$project_dir/$out_dir" -Recurse -Force
